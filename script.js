1959e8e3b0f92770165a6cb57d3b548830bccee3
h <hchenwu02@gmail.com>
2022-05-31 18:31:39 -0400

    Add eraser togger for erasing, rainbow toggler for random colors, and clear to remove all colors from the canvas

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let mousePress = false;
 let eraserToggler = false;
+let rainbowToggler = false;
 
 let setGridSize = (size) => {
     let parent = document.querySelector(".canvas-container");
     document.querySelector(".canvas").remove();
     let gridContainer = document.createElement("div");
+    gridContainer.addEventListener("mousedown", () => mousePress = true);
+    gridContainer.addEventListener("mouseup", () => mousePress = false);
+    // gridContainer.addEventListener("mouseleave", () => mousePress = false);
     gridContainer.classList.add("canvas");
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -16,6 +20,9 @@ let setGridSize = (size) => {
         gridItem.addEventListener("click", () => {
             if(eraserToggler){
                 gridItem.style.cssText = `outline: 1px solid #C5C5C5; background-color: white`;
+            } else if(rainbowToggler){
+                let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                gridItem.style.cssText = `outline: 1px solid #C5C5C5; background-color: #${randomColor}`;
             } else{
                 gridItem.style.cssText = `background-color: ${color_selector.value}`;
             }
@@ -24,6 +31,9 @@ let setGridSize = (size) => {
         gridItem.addEventListener("mousemove", () => {
             if(mousePress && eraserToggler){
                 gridItem.style.cssText = `outline: 1px solid #C5C5C5; background-color: white`;
+            } else if (mousePress && rainbowToggler){
+                let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                gridItem.style.cssText = `outline: 1px solid #C5C5C5; background-color: #${randomColor}`;
             } else if(mousePress && !eraserToggler){
                 gridItem.style.cssText = `background-color: ${color_selector.value}`;
             }
@@ -31,14 +41,11 @@ let setGridSize = (size) => {
         gridContainer.append(gridItem);
     }
 }
-
-let body = document.querySelector("body");
-
+setGridSize(16);
 
 let setMousePressTrue = () => mousePress = true;
 let setMousePressFalse = () => mousePress = false;
-body.onmousedown = setMousePressTrue;
-body.onmouseup = setMousePressFalse;
+
 
 let color_selector = document.getElementById("color-selector");
 let changeBackground = () => {
@@ -62,7 +69,31 @@ slider.oninput = () => {
 let setEraserToggler = () => eraserToggler ? eraserToggler = false : eraserToggler = true;
 
 let eraserButton = document.querySelector(".eraser");
-eraserButton.addEventListener("click", () => setEraserToggler());
+eraserButton.addEventListener("click", () => {
+    if(rainbowToggler){
+        setRainbowToggler();
+        rainbowButton.classList.toggle("active");
+    }
+    setEraserToggler();
+    eraserButton.classList.toggle("active");
+});
 
-setGridSize(16);
+let setRainbowToggler = () => rainbowToggler ? rainbowToggler = false : rainbowToggler = true;
+let rainbowButton = document.querySelector(".rainbow");
+rainbowButton.addEventListener("click", () => {
+    if(eraserToggler){
+        setEraserToggler();
+        eraserButton.classList.toggle("active");
+    }
+    setRainbowToggler();
+    rainbowButton.classList.toggle("active");
+});
 
+
+let clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+    let children = document.querySelector(".canvas").childNodes;
+    children.forEach(child => {
+        child.style.cssText = `outline: 1px solid #C5C5C5; background-color: white`;
+    });
+})
